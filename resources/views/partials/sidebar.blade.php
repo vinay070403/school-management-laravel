d8cec11943c038ca7e8a161b7be2f3ac415b8e8b
vinay chavada <tanish.chavada@gmail.com>
2025-09-18 18:11:06 +0530

    sidebar

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="container-fluid page-body-wrapper">
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#" id="dashboardLink">
+                    <i class="mdi mdi-home menu-icon"></i>
+                    <span class="menu-title">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" id="usersLink">
+                    <i class="mdi mdi-account menu-icon"></i>
+                    <span class="menu-title">Users</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
